a9d324876a33cd92b313354d2f224e3275514e42
ShankarBirTamang <sankar.jt19@gmail.com>
2021-10-11 15:36:21 +0545

    basics

---
+++ 1_Basics1/pass.py
@@ -0,0 +1,15 @@
+for x in range(5):
+    pass
+
+for x in range(5):
+    if(x==3):
+        pass
+    print("Number is ", x)
+
+#empty function
+def func():
+    pass
+
+#empty class
+class myclass:
+    pass
